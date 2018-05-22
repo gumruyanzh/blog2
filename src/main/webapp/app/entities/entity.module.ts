64735fae549f6412548e93866003b12b00153dc3
vagrant <vagrant@vagrant.vm>
2018-05-22 14:49:24 +0000

    added entities

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,15 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { Blog2BlogModule } from './blog/blog.module';
+import { Blog2EntryModule } from './entry/entry.module';
+import { Blog2TagModule } from './tag/tag.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        Blog2BlogModule,
+        Blog2EntryModule,
+        Blog2TagModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
